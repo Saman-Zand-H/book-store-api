baaab27e52b3ecd980e536a7a764dcc06c51a5c4
Saman Zand Haghighi <samanzandh@gmail.com>
2024-08-16 00:54:09 +0330

    Use startup.cs

--- BookStore.Api/Program.cs
+++ BookStore.Api/Program.cs
@@ -1,70 +1,17 @@
-using BookStore.Api.Mapping;
-using BookStore.Api.Settings;
-using BookStore.Api.Repositories;
-using BookStore.Api.Services;
-using BookStore.Api.Data;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-
-var builder = WebApplication.CreateBuilder(args);
-
-BookMappingConfig.RegisterMappings();
-UserMappingConfig.RegisterMappings();
-
-var jwtSettings = new JwtSettings();
-builder.Configuration.Bind("JwtSettings", jwtSettings);
-string? secreteKey = builder.Configuration["SecretKey"];
-
-if (string.IsNullOrEmpty(secreteKey))
+namespace BookStore.Api
 {
-    throw new InvalidOperationException("JWT Secret Key is not configured properly.");
-}
-
-jwtSettings.SecretKey = secreteKey;
-
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.Configure<BookStoreDatabaseSettings>(builder.Configuration.GetSection("BookStoreDatabase"));
-builder.Services.AddSingleton(jwtSettings);
-builder.Services.AddSingleton<MongoDbContext>();
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
+    public class Program
     {
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.SecretKey))
-    };
-});
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IBookRepository, BookRepository>();
-builder.Services.AddScoped<IBookService, BookService>();
-builder.Services.AddScoped<IAuthRepository, AuthRepository>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-app.MapControllers();
-app.Run();
